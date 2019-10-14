f1b4d0f62daf1ed7bc1403aded138f6adf1b45ae
axell-brendow <breno.axel@gmail.com>
2019-10-14 13:52:50 -0300

    Atualiza a documentação da hash

--- HashDinamica/doc/package-search-index.js
+++ HashDinamica/doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"hash"},{"l":"hash.hashs"},{"l":"hash.main"},{"l":"hash.serializaveis"},{"l":"hash.util"}]
+packageSearchIndex = [{"l":"hash"}]
--- HashDinamica/documentar.ps1
+++ HashDinamica/documentar.ps1
@@ -1,4 +1,4 @@
-#javadoc -d ./doc -sourcepath ./src -subpackages hash
-javadoc -d ./doc src/*.java
+javadoc -d ./doc -sourcepath ./src -subpackages hash
+#javadoc -d ./doc src/*.java
 
 rm *.log
