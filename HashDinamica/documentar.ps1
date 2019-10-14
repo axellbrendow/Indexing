ba3069545003c8861f08b79afe891c0c7f79b85b
axell-brendow <breno.axel@gmail.com>
2019-10-14 15:03:01 -0300

    Conserta encoding da documentação

--- HashDinamica/documentar.ps1
+++ HashDinamica/documentar.ps1
@@ -1,4 +1,4 @@
-javadoc -d ./doc -sourcepath ./src -subpackages hash
+javadoc -d ./doc -sourcepath ./src -subpackages hash -encoding UTF-8 -docencoding UTF-8
 #javadoc -d ./doc src/*.java
 
 rm *.log
